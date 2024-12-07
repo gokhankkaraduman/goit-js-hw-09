3dfdb8bdc5c5f61debbdb7c294c7361c7fd38692
gokhankkaraduman <gokhan.kkaraduman3@gmail.com>
2024-12-07 15:06:01 +0300

    ı finished all projecet

--- src/public/js/gallery.js
+++ src/public/js/gallery.js
@@ -91,3 +91,21 @@ new SimpleLightbox(".gallery a", {
     captionDelay: 250,
     captionPosition: "bottom",
 });
+
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const numberOfDots = 100;
+    const body = document.body;
+
+    for (let i = 0; i < numberOfDots; i++) {
+      const dot = document.createElement("div");
+      dot.classList.add("dot");
+      dot.style.width = Math.random() * 3 + "px";
+      dot.style.height = dot.style.width;
+      dot.style.left = Math.random() * 100 + "vw";
+      dot.style.animationDuration = Math.random() * 5 + 4 + "s";
+      dot.style.animationDelay = Math.random() * 2 + "s";
+      body.appendChild(dot);
+    }
+  });
